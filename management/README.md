c73d6b2d9d0b45ebee975a42e3e414fd3e7dd7d8
Christian Castelli <christian.castelli@docebo.com>
2025-12-22 10:34:01 +0100

    Modified deployment script and README with conf for NGINX

--- management/README.md
+++ management/README.md
@@ -10,9 +10,11 @@ the following tasks:
 In order to correctly deploy the files, you must execute:
 
 ```
-./bin/chess deploy -e <environment>
+./bin/chess deploy -e <environment> <type>
 ```
-`-e|--environment`: may be `local` or `production`. You must define
+`<type>`: it may be `frontend`, `backend` or `both` (default)  
+`--dry-run`: it will print the operation it will do without doing them  
+`-e|--environment`: may be `local` or `remote`. You must define
 the environment variables in an .env file placed in the root of this
 project following the examples contained in `.env.dist`.  
 For example you can `cp .env.dist .env` and then edit its content. The
@@ -41,6 +43,17 @@ For apache, the minimal configuration is the following:
 </VirtualHost>
 ```
 
+for NGINX:
+
+```
+  root "{{root}}/public"; 
+  index index.html index.php;
+  
+  location / {
+    try_files $uri $uri/ /index.php?$query_string;
+  }
+```
+
 For the manual deployment, assuming `/opt/homebrew/var/www/scacchilatorre` is the public directory (we will
 refer to it with ROOT_DIR), you need to:
 1. for the backend project, move all the files inside `ROOT_DIR`
@@ -55,4 +68,4 @@ For the backend, you can omit all the file with the following extensions:
 * the directories `tests`, `logs`
 
 The directory vendor can be omitted if you have SSH access and have
-composer installed in the user terminal.cd 
+composer installed in the user terminal.
--- management/src/Commands/Deploy.php
+++ management/src/Commands/Deploy.php
@@ -2,23 +2,37 @@
 
 namespace Scacchilatorre\Management\Commands;
 
+use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
-
 #[AsCommand(
     name: 'deploy',
     description: 'Deploy the files to the specified server',
     help: <<<TXT
         ./bin/chess deploy -e local: deploy the files to the local server (this is the default)
         ./bin/chess deploy -e remote: deploy the files to the remote server
+        ./bin/chess deploy -e remote --dry-run: will print the commands for deploying remotely the app
+        ./bin/chess deploy -e remote frontend: will just deploy remotely the frontend
 TXT
 )]
 class Deploy extends Command
 {
     private SymfonyStyle $io;
     private string $environment;
+    private array $availableDeployTypes = ['frontend', 'backend', 'both'];
+    private bool $dryRun = false;
+
+    private function getTitle(string $type): string
+    {
+        $title = 'Deploying ';
+        $title .= $type === 'both' ? 'both frontend and backend' : $type;
+        $title .= " to " . $this->environment;
+        $title .= $this->dryRun ? ' (dry-run)' : '';
+
+        return $title;
+    }
 
     public function __invoke(
         SymfonyStyle $io,
@@ -28,28 +42,41 @@ class Deploy extends Command
             shortcut: 'e',
             suggestedValues: ['local', 'remote']
         )] string $environment = 'local',
+        #[Argument(
+            description: 'Deploys one or more specific parts (default: both)',
+            name: 'type',
+            suggestedValues: ['frontend', 'backend', 'both']
+        )] string $type = 'both',
         #[Option(
-            description: 'Just deploys the frontend (default: false)',
-            name: 'frontend',
-            shortcut: 'f',
-        )] bool $onlyFrontend = false,
-        #[Option(
-            description: 'Just deploys the backend (default: false)',
-            name: 'backend',
-            shortcut: 'b',
-        )] bool $onlyBackend = false,
+            description: 'Dry run (default: false)',
+            name: 'dry-run'
+        )] bool $dryRun = false,
     ): int
     {
         $this->io = $io;
         $this->environment = $environment;
-        $io->title('Deploying to ' . $environment);
+        $this->dryRun = $dryRun;
 
-        $rootDir = getcwd();
-        if ($this->deployBackend($rootDir) !== Command::SUCCESS) {
+        if (!in_array($type, $this->availableDeployTypes)) {
+            $io->error("Invalid Deploy type '$type'");
             return Command::FAILURE;
         }
 
-        if ($this->deployFrontend($rootDir) !== Command::SUCCESS) {
+        $io->title($this->getTitle($type));
+
+        $rootDir = getcwd();
+
+        if (
+            in_array($type, ['both', 'backend']) &&
+            $this->deployBackend($rootDir) !== Command::SUCCESS
+        ) {
+           return Command::FAILURE;
+        }
+
+        if (
+            in_array($type, ['both', 'frontend']) &&
+            $this->deployFrontend($rootDir) !== Command::SUCCESS
+        ) {
             return Command::FAILURE;
         }
 
@@ -57,61 +84,225 @@ class Deploy extends Command
         return Command::SUCCESS;
     }
 
-    private function deployBackend(string $rootDir): int
+    private function deployLocalBackend(): int
     {
-        chdir($rootDir . DIRECTORY_SEPARATOR . '../backend');
-        $this->io->writeln('Current directory: ' . getcwd());
-
+        $this->io->writeln('Copying backend files to local server...');
         $output = [];
         $retval = null;
 
-        if ($this->environment === 'local') {
-            $this->io->writeln('Copying application files...');
-            exec('cp -R . ' . $_ENV['LOCAL_DEPLOY_PATH'] . ' 2>&1', $output, $retval);
-            if ($retval !== Command::SUCCESS) {
-                $this->io->error('Failed to copy application files: ' . $output);
-                return Command::FAILURE;
-            }
-        } else {
-            // @TODO
+        $command = 'cp -R . ' . $_ENV['LOCAL_DEPLOY_PATH'] . ' 2>&1';
+        if ($this->dryRun) {
+            $this->io->writeln($command);
+            return Command::SUCCESS;
+        }
+
+        exec($command, $output, $retval);
+        if ($retval !== Command::SUCCESS) {
+            $this->io->error('Failed to copy application files: ' . join("\n", $output));
+            return Command::FAILURE;
         }
 
         return Command::SUCCESS;
     }
 
-    private function deployFrontend(string $rootDir): int
+    private function deployRemoteBackend(): int
     {
-        chdir($rootDir . DIRECTORY_SEPARATOR . '../frontend');
-        $this->io->writeln('Current directory: ' . getcwd());
+        $this->io->writeln('Copying backend files to remote server...');
+
+        // Rsync arguments explained:
+        // -a: Archive mode (recurse, preserve permissions, times, etc.)
+        // -v: Verbose output
+        // -z: Compress file data during the transfer
+        // -e: Specify the remote shell (needed to set the custom SSH port)
+        // --exclude: Prevent uploading unnecessary/sensitive files
+        $excludeList = [
+            ".git",
+            ".github",
+            "data",
+            "*.xml",
+            "*.dist",
+            "*.bak",
+            ".phpunit.result.cache",
+            ".env",
+            "tests",
+            "vendor",
+            "*.md",
+            ".coveralls.yml"
+        ];
+
+        $excludeParams = array_map(function ($item) {
+            return '--exclude=' . escapeshellarg($item);
+        }, $excludeList);
+        $excludeString = implode(' ', $excludeParams);
+
+        $command = sprintf(
+            'rsync -avz -e "ssh -p %s" %s . %s@%s:%s 2>&1',
+            $_ENV['FTP_PORT'],
+            $excludeString,
+            $_ENV['FTP_USER'],
+            $_ENV['FTP_HOST'],
+            $_ENV['FTP_REMOTE_PATH']
+        );
+
+        if ($this->dryRun) {
+            $this->io->writeln($command);
+        } else {
+            $output = [];
+            $retval = null;
+            exec($command, $output, $retval);
+
+            if ($retval !== Command::SUCCESS) {
+                $this->io->error('Rsync failed for backend:');
+                $this->io->writeln($output);
+                return Command::FAILURE;
+            }
+
+            $this->io->success('Backend files copied successfully.');
+        }
+
+
+        // 2. Install Dependencies Remotely
+        $this->io->writeln('Running "composer install" on remote server...');
+
+        // We ssh into the server, cd to the directory, and run composer.
+        // --no-dev: Don't install development dependencies (phpunit, etc) on prod
+        // --optimize-autoloader: Speeds up class loading
+        $composerCommand = sprintf(
+            'ssh -p %s %s@%s "cd %s && composer install --no-dev --optimize-autoloader --no-interaction 2>&1"',
+            $_ENV['FTP_PORT'],
+            $_ENV['FTP_USER'],
+            $_ENV['FTP_HOST'],
+            $_ENV['FTP_REMOTE_PATH']
+        );
 
         $output = [];
         $retval = null;
-        exec('ng build --optimization --delete-output-path 2>&1', $output, $retval);
-        if ($retval === Command::SUCCESS) {
-            $this->io->writeln('Frontend successfully built');
-            foreach ($output as $line) {
-                if (
-                    str_contains(strtolower($line), 'warning') ||
-                    str_contains(strtolower($line), 'error')
-                ) {
-                    $this->io->writeln($line);
-                }
-            }
+        if ($this->dryRun) {
+            $this->io->writeln($composerCommand);
         } else {
-            $this->io->error('Frontend build failed [' . $retval . ']');
-            $this->io->writeln(join("\n", $output));
+            exec($composerCommand, $output, $retval);
+
+            if ($retval !== Command::SUCCESS) {
+                $this->io->error('Remote composer install failed:');
+                $this->io->writeln($output);
+                return Command::FAILURE;
+            }
+
+            $this->io->success('Composer dependencies installed successfully.');
         }
 
+        return Command::SUCCESS;
+    }
+
+    private function deployBackend(string $rootDir): int
+    {
+        $this->io->section('Backend');
+        chdir($rootDir . DIRECTORY_SEPARATOR . '../backend');
+        $this->io->writeln('Current directory: ' . getcwd());
+
         if ($this->environment === 'local') {
-            exec('cp -R dist/browser/* ' . $_ENV['LOCAL_DEPLOY_PATH'] . DIRECTORY_SEPARATOR . 'public 2>&1', $output, $retval);
-            if ($retval === Command::SUCCESS) {
-                $this->io->writeln('Frontend files successfully copied');
-            }
+            return $this->deployLocalBackend();
+        }
+
+        return $this->deployRemoteBackend();
+    }
+
+    private function deployLocalFrontend(): int
+    {
+        $this->io->writeln('Copying backend files to local server...');
+        $output = [];
+        $retval = null;
+
+        $command = 'cp -R dist/browser/* ' . $_ENV['LOCAL_DEPLOY_PATH'] . DIRECTORY_SEPARATOR . 'public 2>&1';
+        if ($this->dryRun) {
+            $this->io->writeln($command);
+            return Command::SUCCESS;
+        }
+
+        exec($command,$output, $retval);
+        if ($retval === Command::SUCCESS) {
+            $this->io->writeln('Frontend files successfully copied');
         } else {
             $this->io->error('Frontend files copy failed [' . $retval . ']');
             $this->io->writeln(join("\n", $output));
         }
 
         return $retval;
+    }
+
+    private function deployRemoteFrontend(): int
+    {
+        $this->io->writeln('Copying backend files to remote server...');
+
+        $output = [];
+        $retval = null;
+
+        // Ensure we target the 'public' folder on the remote,
+        // similar to how the local deploy targets LOCAL_DEPLOY_PATH/public
+        $remotePublicPath = rtrim($_ENV['FTP_REMOTE_PATH'], '/') . '/public';
+
+        // Note on the source path "dist/browser/":
+        // In rsync, the trailing slash is CRITICAL.
+        // "dist/browser/" means "copy the CONTENTS of this folder".
+        // "dist/browser" (no slash) means "copy the folder ITSELF".
+        // We want the contents to go into the remote 'public' folder.
+        $command = sprintf(
+            'rsync -avz -e "ssh -p %s" dist/browser/ %s@%s:%s 2>&1',
+            $_ENV['FTP_PORT'],
+            $_ENV['FTP_USER'],
+            $_ENV['FTP_HOST'],
+            $remotePublicPath
+        );
+
+        if ($this->dryRun) {
+            $this->io->writeln($command);
+            return Command::SUCCESS;
+        }
+        exec($command, $output, $retval);
+
+        if ($retval !== Command::SUCCESS) {
+            $this->io->error('Rsync failed for frontend:');
+            $this->io->writeln($output);
+            return Command::FAILURE;
+        }
+
+        $this->io->success('Frontend files copied successfully.');
+        return Command::SUCCESS;
+    }
+
+    private function deployFrontend(string $rootDir): int
+    {
+        $this->io->section('Frontend');
+        chdir($rootDir . DIRECTORY_SEPARATOR . '../frontend');
+        $this->io->writeln('Current directory: ' . getcwd());
+
+        $command = 'ng build --optimization --delete-output-path 2>&1';
+        if ($this->dryRun) {
+            $this->io->writeln($command);
+        } else {
+            $output = [];
+            $retval = null;
+            exec($command, $output, $retval);
+            if ($retval === Command::SUCCESS) {
+                $this->io->writeln('Frontend successfully built');
+                foreach ($output as $line) {
+                    if (
+                        str_contains(strtolower($line), 'warning') ||
+                        str_contains(strtolower($line), 'error')
+                    ) {
+                        $this->io->writeln($line);
+                    }
+                }
+            } else {
+                $this->io->error('Frontend build failed [' . $retval . ']');
+                $this->io->writeln(join("\n", $output));
+            }
+        }
+
+        if ($this->environment === 'local') {
+            return $this->deployLocalFrontend($rootDir);
+        }
+
+        return $this->deployRemoteFrontend($rootDir);
     }
 }
